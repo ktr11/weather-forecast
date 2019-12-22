e024c7e839beccd13f6a35158977e7e72b6c8a71
ktr11 <kenmcz315@gmail.com>
2019-12-22 20:54:28 +0900

    htmlの共通化

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,34 +1,5 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>Weather Forecast</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <style>
-        html,
-        body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Raleway', sans-serif;
-            font-weight: 100;
-            height: 100vh;
-            margin: 0;
-        }
-    </style>
-    <link href="{{ url('/css/app.css') }}" rel="stylesheet" type="text/css">
-</head>
-
-<body>
-    <h1>Weather Forecast</h1>
+@extends('common')
+@section('contents')
     @if ($errors->any())
     <div class="alert alert-danger">
         <ul>
@@ -56,6 +27,4 @@
             </div>
         </div>
     </form>
-</body>
-
-</html>
+@endsection
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,34 +1,10 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>Weather Forecast</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <style>
-        html,
-        body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Raleway', sans-serif;
-            font-weight: 100;
-            height: 100vh;
-            margin: 0;
-        }
-    </style>
-</head>
-
-<body>
-    <h1>Weather Forecast</h1>
-    <table>
+@extends('common')
+@section('contents')
+    <table class="table">
+        <colgroup>
+            <col width="30%">
+            <col width="70%">
+        </colgroup>
         <tr>
             <th>date</th>
             <td>{{ date('Y/m/d') }}</td>
@@ -49,6 +25,4 @@
             <td>{{ $record->min_temp }}</td>
         </tr>
     </table>
-</body>
-
-</html>
+@endsection
