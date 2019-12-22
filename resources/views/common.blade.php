6700f32b3cb9b54421b477817d9485d424bddbb3
ktr11 <kenmcz315@gmail.com>
2019-12-22 23:58:45 +0900

    アプリのタイトルを設定ファイルからとるように変更

--- resources/views/common.blade.php
+++ resources/views/common.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Weather Forecast</title>
+    <title>{{config('app.name')}}</title>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     <!-- Styles -->
@@ -15,7 +15,7 @@
 </head>
 
 <body>
-    <h1>Weather Forecast</h1>
+    <h1>{{config('app.name')}}</h1>
     @yield('contents') 
 </body>
 
