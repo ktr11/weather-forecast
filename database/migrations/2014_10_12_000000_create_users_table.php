1fe707e3a82ca91b82fc71ea347632fa4177e992
ktr11 <kenmcz315@gmail.com>
2019-12-22 20:06:38 +0900

    個別の設定箇所を削除

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name', 256);
-            $table->string('email', 256)->unique();
-            $table->string('password', 256);
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
